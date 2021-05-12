b94a06b603f798c095501b131a331a17a7054483
Ibrahim Chougrani <ichougra@z3r4p5.42lyon.fr>
2021-05-12 17:23:51 +0200

    create file

--- philo_three/main.c
+++ philo_three/main.c
@@ -5,42 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ichougra <ichougra@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/12 14:08:44 by ichougra          #+#    #+#             */
-/*   Updated: 2021/05/12 17:20:15 by ichougra         ###   ########lyon.fr   */
+/*   Created: 2021/05/12 17:22:29 by ichougra          #+#    #+#             */
+/*   Updated: 2021/05/12 17:22:30 by ichougra         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <pthread.h>
-
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void    *funct1(void *arg)
-{
-    int i = 0;
-
-    while (i < 10)
-    {
-        printf("thread 1: %d\n", i);   
-        i++;
-    }
-    return (NULL);
-}
-
-int main(int ac, char **av)
-{
-    pthread_t t1;
-
-    
-
- 
-    pthread_create(&t1, NULL, funct1, NULL);
-
-
-
-
-    pthread_join(t1, NULL);
-
-    return (0);
-}
